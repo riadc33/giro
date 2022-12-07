f6205932377ffeec903c03724ab41b19eec3b6d7
riadc33 <riadc33@gmail.com>
2022-12-07 11:49:29 -0500

    fix

--- App.js
+++ App.js
@@ -1,19 +1,134 @@
 import { StatusBar } from 'expo-status-bar';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Gesture, GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler';
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import BrackGroundGradient from './src/components/BackgroundGradient'
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window')
 const HEIGHT = 256
 const WIDTH = SCREEN_WIDTH * 0.9;
+const WIDTH_CARD = WIDTH - 5;
+const HEIGHT_CARD = HEIGHT - 5;
 
 export default function App() {
+  const rotateX = useSharedValue(0);
+  const rotateY = useSharedValue(0);
+
+
+  const gestures = Gesture.Pan().runOnJS(true).onBegin((event) => {
+    rotateX.value = withTiming(interpolate(
+      event.y,
+      [0, HEIGHT_CARD],
+      [10, -10],
+      Extrapolate.CLAMP))
+    rotateY.value = withTiming(interpolate(
+      event.x,
+      [0, WIDTH_CARD],
+      [-10, 10],
+      Extrapolate.CLAMP))
+  })
+    .onUpdate((event) => {
+      rotateX.value = withTiming(interpolate(
+        event.y,
+        [0, HEIGHT_CARD],
+        [10, -10],
+        Extrapolate.CLAMP))
+      rotateY.value = withTiming(interpolate(
+        event.x,
+        [0, WIDTH_CARD],
+        [-10, 10],
+        Extrapolate.CLAMP))
+
+    }).onFinalize(() => {
+      rotateX.value = withTiming(0)
+      rotateY.value = withTiming(0)
+
+    })
+
+  const rStyle = useAnimatedStyle(() => {
+
+    const rotateXvalue = `${rotateX.value}deg`
+    const rotateYvalue = `${rotateY.value}deg`
+
+    return {
+      transform: [{
+        perspective: 300
+      },
+      { rotateX: rotateXvalue },
+      { rotateY: rotateYvalue }
+      ]
+    }
+  }, [])
+
+
   return (
-    <View style={styles.container}>
-      <BrackGroundGradient width={WIDTH} height={HEIGHT} />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+
+      <View style={styles.container}>
+        <BrackGroundGradient width={WIDTH} height={HEIGHT} />
+        <GestureDetector gesture={gestures}>
+          <Animated.View style={[{
+            height: HEIGHT_CARD,
+            width: WIDTH_CARD,
+            backgroundColor: 'black',
+            position: 'absolute',
+            borderRadius: 20,
+            zIndex: 300,
+          },
+            rStyle
+          ]}>
+            <View
+              style={{
+                position: 'absolute',
+                bottom: '10%',
+                left: '10%',
+                flexDirection: 'row',
+              }}>
+              <View
+                style={{
+                  height: 50,
+                  aspectRatio: 1,
+                  borderRadius: 25,
+                  backgroundColor: '#272F46',
+                }}
+              />
+              <View
+                style={{
+                  flexDirection: 'column',
+                  marginLeft: 10,
+                  justifyContent: 'space-around',
+                }}>
+                <View
+                  style={{
+                    height: 20,
+                    width: 80,
+                    borderRadius: 25,
+                    backgroundColor: '#272F46',
+                  }}
+                />
+                <View
+                  style={{
+                    height: 20,
+                    width: 80,
+                    borderRadius: 25,
+                    backgroundColor: '#272F46',
+                  }}
+                />
+              </View>
+            </View>
+          </Animated.View>
+        </GestureDetector>
+
+      </View>
+    </GestureHandlerRootView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'black',
@@ -22,3 +137,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
--- src/components/BackgroundGradient.js
+++ src/components/BackgroundGradient.js
@@ -1,19 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Canvas, RoundedRect, SweepGradient, vec } from '@shopify/react-native-skia';
+import {
+    BlurMask,
+    Canvas,
+    RoundedRect,
+    SweepGradient,
+    useSharedValueEffect,
+    useValue,
+    vec,
+} from '@shopify/react-native-skia';
+import React, { useEffect } from 'react';
+import {
+    useSharedValue,
+    withRepeat,
+    withTiming,
+} from 'react-native-reanimated';
 
 export default function backgroundGradient({ width, height }) {
 
+
+    const canvasPadding = 40;
+    const rValue = useSharedValue(0);
+    const skValue = useValue(0);
+
+    useEffect(() => {
+        rValue.value = withRepeat(withTiming(10, { duration: 2000 }), -1, true);
+    }, [rValue]);
+
+    useSharedValueEffect(() => {
+        skValue.current = rValue.value;
+    }, rValue);
+
     return (
         <>
-            <Canvas style={{ width, height, }}>
-                <RoundedRect x={0} y={0} width={width} height={height} color={'white'} r={20} >
-
-                    <SweepGradient c={vec(width / 2, height / 2)} colors={['cyan', 'magenta', 'yellow', 'cyan']} />
+            <Canvas
+                style={{
+                    width: width + canvasPadding,
+                    height: height + canvasPadding,
+                }}>
+                <RoundedRect
+                    x={canvasPadding / 2}
+                    y={canvasPadding / 2}
+                    width={width}
+                    height={height}
+                    color={'white'}
+                    r={20}>
+                    <SweepGradient
+                        c={vec((width + canvasPadding) / 2, (height + canvasPadding) / 2)}
+                        colors={['cyan', 'magenta', 'yellow', 'cyan']}
+                    />
+                    <BlurMask blur={skValue} style={'solid'} />
                 </RoundedRect>
             </Canvas>
         </>
     )
-}
 
-const styles = StyleSheet.create({})
+}
